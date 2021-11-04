11bd4d6fd88cf48007290e22a288deece4e753b1
weidehai <243395655@qq.com>
2021-11-04 16:35:38 +0800

    20211104

---
+++ pages/test/test.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<image src="/static/image/pic1.png" style="height:400px;width:300px"></image>
+		<image src="/static/image/pic1.png" style="height:320px;width:300px"></image>
+		<image src="/static/image/pic1.png" style="height:250px;width:300px"></image>
+		<image src="/static/image/pic1.png" style="height:100px;width:300px"></image>
+		<view style="display: inline-block;">k</view>
+		x
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	view{
+		border: 1px solid red;
+	}
+	image{
+		vertical-align: middle;
+	}
+</style>
